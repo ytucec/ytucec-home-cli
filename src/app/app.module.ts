23186d1eb4473ccb26108560fb0527017f8a4259
dorukBen <dorukerena@gmail.com>
2018-04-14 22:22:40 +0300

    Add routing
    Home component and Routing module created.
    Base routes configurated.
    Router-outlet imported to app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RoutingModule } from './module/routing/routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
